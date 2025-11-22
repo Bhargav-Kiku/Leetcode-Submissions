8e7d99d85677f4d4ad03337e1efc7e6177d668bf
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-22 14:17:18 +0530

    Time: 75 ms (39.27%), Space: 20.5 MB (68.06%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
@@ -4,8 +4,9 @@ class Solution:
         v = 1 << m - 1
         mp = {}
         for row in matrix:
-            p = ''.join(map(str, row))
-            op = ''.join([(str(1-x)) for x in row])
-            mp[p] = mp.get(p,0) + 1
+            if row[0] == 0:
+                op = ''.join([(str(1-x)) for x in row])
+            else:
+                op = ''.join([(str(x)) for x in row])
             mp[op] = mp.get(op,0) + 1
         return max(mp.values())
