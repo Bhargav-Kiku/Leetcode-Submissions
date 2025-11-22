b56620a799137077f3fc88fafa13184e6dde2606
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-22 20:03:27 +0530

    Time: 1040 ms (64.4%), Space: 39.9 MB (60.4%) - LeetHub

---
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minSideJumps(self, obstacles: List[int]) -> int:
+        dp = [float('inf')] * 4
+        dp[1], dp[2], dp[3] = 1, 0, 1
+        for x in obstacles:
+            if x:
+                dp[x] = float('inf')            
+            for j in (1,2,3):
+                if j != x:
+                    dp[j] = min(dp[j], dp[(j%3)+1] + 1, dp[(j+1)%3+1] + 1)
+        return min(dp[1], dp[2], dp[3])
