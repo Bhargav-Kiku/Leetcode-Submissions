7415c7f6dc9eb7725ac0b12a01c732a64a74a462
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-24 03:19:36 +0530

    Time: 55 ms (76.86%), Space: 31.9 MB (83.53%) - LeetHub

---
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        e = -1
+        c = 0
+        for i in nums:
+            if e == i:
+                c += 1
+            else:
+                c -= 1
+                if c <= 0:
+                    e = i
+                    c = 1
+        c = nums.count(e)
+        nc = 0
+        n = len(nums)
+        for i in range(n-1):
+            if nums[i] == e:
+                nc += 1
+            if nc > (i+1)/2 and (c-nc) > (n-i-1)/2:
+                return i
+            # print(i,nc)
+        return -1
