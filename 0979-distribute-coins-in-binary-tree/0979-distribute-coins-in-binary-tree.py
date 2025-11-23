a1feff1b22949f413148c6052ab33e3cf5a0cd7b
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-24 01:15:59 +0530

    Time: 0 ms (100%), Space: 17.9 MB (29.62%) - LeetHub

--- 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.py
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.py
@@ -7,12 +7,12 @@
 class Solution:
     def distributeCoins(self, root: Optional[TreeNode]) -> int:
         self.res = 0
-        def dfs(root,p):
+        def dfs(root):
             if not root:
                 return 0
-            l = dfs(root.left,root)
-            r = dfs(root.right,root)
+            l = dfs(root.left)
+            r = dfs(root.right)
             self.res += abs(l) + abs(r)
             return root.val - 1 + l + r
-        dfs(root,None)
+        dfs(root)
         return self.res
