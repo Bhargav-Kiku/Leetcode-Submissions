fc4c55a9aead7ff9d9f7311170552acdaf0da2a8
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-24 03:58:08 +0530

    Time: 127 ms (80.52%), Space: 62.5 MB (72.94%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -0,0 +1,19 @@
+class Solution:
+    def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
+        mps = defaultdict(list)
+        for i, j in adjacentPairs:
+            mps[i].append(j)
+            mps[j].append(i)
+        res = []
+        vis = set()
+        for i, j in mps.items():
+            if len(j) == 1:
+                res.append(i)
+                vis.add(i)
+                break
+        while len(res) < len(mps):
+            for x in mps[res[-1]]:
+                if x not in vis:
+                    vis.add(x)
+                    res.append(x)
+        return res
