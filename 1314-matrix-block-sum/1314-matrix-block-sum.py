076d9f8ca4fcd6ed04976e37c210a6cd555f5063
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-24 02:07:25 +0530

    Time: 23 ms (24.83%), Space: 19.2 MB (59.73%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.py
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -1,10 +1,27 @@
 class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
         m, n = len(mat), len(mat[0])
-        ans = [[0] * n for _ in range(m)]
+        for r in mat:
+            print(*r)
+        print()
+        nmat = [[0] * n for _ in range(m)]
+        for i in range(m):
+            sc = 0
+            for j in range(n):
+                sc += mat[i][j]
+                nmat[i][j] += sc
+                if i > 0:
+                    nmat[i][j] += nmat[i-1][j]
+        res = [[0] * n for _ in range(m)]
         for i in range(m):
             for j in range(n):
-                for x in range(max(0,i-k),min(m-1,i+k)+1):
-                    for y in range(max(0,j-k),min(n-1,j+k)+1):
-                        ans[i][j] += mat[x][y]
-        return ans
+                mnr, mxr = max(0, i - k), min(m - 1, i + k)
+                mnc, mxc = max(0, j - k), min(n - 1, j + k)
+                res[i][j] = nmat[mxr][mxc]
+                if mnr > 0:
+                    res[i][j] -= nmat[mnr-1][mxc]
+                if mnc > 0:
+                    res[i][j] -= nmat[mxr][mnc-1]
+                if mnr > 0 and mnc > 0:
+                    res[i][j] += nmat[mnr-1][mnc-1]
+        return res
