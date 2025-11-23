fb2ba412b69e854c748d246622f75c203d511a8b
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-24 02:09:35 +0530

    Time: 15 ms (80.03%), Space: 18.9 MB (90.44%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.py
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
         m, n = len(mat), len(mat[0])
-        for r in mat:
-            print(*r)
-        print()
+        # for r in mat:
+        #     print(*r)
+        # print()
         nmat = [[0] * n for _ in range(m)]
         for i in range(m):
             sc = 0
@@ -12,6 +12,9 @@ class Solution:
                 nmat[i][j] += sc
                 if i > 0:
                     nmat[i][j] += nmat[i-1][j]
+        # for r in nmat:
+        #     print(*r)
+        # print()
         res = [[0] * n for _ in range(m)]
         for i in range(m):
             for j in range(n):
