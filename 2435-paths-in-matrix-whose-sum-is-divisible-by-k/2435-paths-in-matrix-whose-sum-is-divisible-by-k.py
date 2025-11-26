779c5ddf431c9d4a8d6fa20bc0e1900f43473d9a
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-26 14:32:47 +0530

    Time: 1682 ms (73.99%), Space: 98.6 MB (47.09%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.py
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.py
@@ -2,12 +2,12 @@ MOD = 1000000007
 class Solution:
     def numberOfPaths(self, grid: List[List[int]], k: int) -> int:
         n, m = len(grid), len(grid[0])
-        if m == 1 or n == 1:
-            if sum(num for act_row in grid for num in act_row) % k:
-                return 0
-            return 1
-        if k == 1:
-            return comb(m + n - 2, n - 1) % MOD
+        # if m == 1 or n == 1:
+        #     if sum(num for act_row in grid for num in act_row) % k:
+        #         return 0
+        #     return 1
+        # if k == 1:
+        #     return comb(m + n - 2, n - 1) % MOD
         dp = [[[0] * k for _ in range(m)] for _ in range(n)]
         dp[0][0][grid[0][0] % k] = 1
         for i in range(n):
