75f2cab04e650ba42795338bb3339c0b052e3c18
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-26 14:32:37 +0530

    Time: 1242 ms (89.69%), Space: 98.6 MB (57.4%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.py
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.py
@@ -8,13 +8,18 @@ class Solution:
             return 1
         if k == 1:
             return comb(m + n - 2, n - 1) % MOD
-        dp = [[[0] * k for _ in range(m+1)] for _ in range(n+1)]
-        dp[1][1][grid[0][0] % k] = 1
+        dp = [[[0] * k for _ in range(m)] for _ in range(n)]
+        dp[0][0][grid[0][0] % k] = 1
         for i in range(n):
             for j in range(m):
                 if i == 0 and j == 0: continue
                 v = grid[i][j] % k
                 for q in range(k):
-                    f, s = dp[i+1][j][(k+q-v) % k], dp[i][j+1][(k+q-v) % k]
-                    dp[i+1][j+1][q] = ((f) + (s)) % MOD
+                    r = (v + q) % k
+                    res = 0
+                    if i > 0:
+                        res += dp[i-1][j][q]
+                    if j > 0:
+                        res += dp[i][j-1][q]
+                    dp[i][j][r] = (dp[i][j][r] + res) % MOD
         return dp[-1][-1][0]
