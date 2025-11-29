2cca3cc0985baba784cd4db5e6e050e8ffee28f8
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-30 00:16:03 +0530

    Time: 2 ms (6.49%), Space: 32.7 MB (12.65%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) {
+        int res = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if ((i & 1) == 0) {
+                res += nums[i];
+            } else {
+                res -= nums[i];
+            }
+        }
+        return res;
+    }
+};
