e8c549e9c9ab5520999b4dea5aa570ea3df0de2e
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-30 00:15:25 +0530

    Time: 1 ms (94.57%), Space: 46.3 MB (6.63%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if ((i & 1) == 0) {
+                res += nums[i];
+            } else {
+                res -= nums[i];
+            }
+        }
+        return res;
+    }
+}
