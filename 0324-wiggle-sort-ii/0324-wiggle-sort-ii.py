04be1af380aa6632eb4f56a8e53fa300e59e4510
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-29 21:45:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        for i in range(n - 1):
+            if (i % 2 == 0 and nums[i] > nums[i + 1]) or (i % 2 == 1 and nums[i] < nums[i + 1]):
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
