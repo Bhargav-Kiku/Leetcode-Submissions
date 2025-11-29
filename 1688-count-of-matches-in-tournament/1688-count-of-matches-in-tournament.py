7c7969e05e114ebe5b75cb75d9129045858242a8
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-30 02:06:34 +0530

    Time: 37 ms (58.48%), Space: 17.9 MB (28.84%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -1,7 +1,3 @@
 class Solution:
     def numberOfMatches(self, n: int) -> int:
-        res = 0
-        while n > 1:
-            res += n // 2
-            n = ceil(n/2)
-        return res
+        return n - 1
