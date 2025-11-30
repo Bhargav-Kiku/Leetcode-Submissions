9df3a61748368dc22d289af3cbb9862bcec2b1f3
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-30 21:03:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.py
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.py
@@ -1,8 +1,9 @@
 class Solution:
     def countElements(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        if k == 0: return 0
         f = Counter(nums)
         li = sorted(list(f.keys()))
-        n = len(nums)
         res = 0
         for i in li:
             n -= f[i]
