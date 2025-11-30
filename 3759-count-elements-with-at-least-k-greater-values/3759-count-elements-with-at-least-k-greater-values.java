59424de53d253543f186b4833f8aa808f2e6b89d
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-11-30 21:24:11 +0530

    Time: 41 ms (100%), Space: 134.2 MB (0%) - LeetHub

--- 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.java
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.java
@@ -2,22 +2,14 @@ class Solution {
     public int countElements(int[] nums, int k) {
         int n = nums.length;
         if (k == 0) return n;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i: nums) {
-            map.put(i, map.getOrDefault(i,0) + 1);
-        }
-        List<Integer> ue = new ArrayList<>(map.keySet());
-        Collections.sort(ue);
+        Arrays.sort(nums);
 
-        int rm = n;
-        int res = 0;
+        int idx = n - k - 1;
 
-        for (int x: ue) {
-            rm -= map.get(x);
-            if (rm >= k) res += map.get(x);
-            else break;
+        while (idx >= 0 && nums[idx] == nums[idx+1]) {
+            idx --;
         }
 
-        return res;
+        return idx + 1;
     }
 }
