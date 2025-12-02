dc1fcd5ae547df1225ee31e432b56eb07991de8b
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-12-02 12:17:50 +0530

    Time: 63 ms (98.62%), Space: 67.4 MB (10.41%) - LeetHub

--- 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.py
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.py
@@ -1,11 +1,13 @@
+MOD = 1000000007
 class Solution:
     def countTrapezoids(self, points: List[List[int]]) -> int:
-        c = 0
-        m = 10 ** 9 + 7
-        f = Counter([y for x,y in points])
-        b = [i * (i - 1) // 2 for i in f.values() if i >= 2]
-        tb = sum(b)
-        for nb in b:
-            tb -= nb
-            c += nb * tb
-        return c % m
+        mp = defaultdict(int)
+        for i, j in points:
+            mp[j] += 1
+        li = [x * (x - 1) // 2 for x in mp.values() if x != 1]
+        tl = sum(li)
+        res = 0
+        for l in li:
+            tl -= l
+            res += l * tl
+        return res % MOD
