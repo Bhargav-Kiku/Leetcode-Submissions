16ffa7e5f6d2b79b3dfd9f7c31ce66dbc0d13b34
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-12-04 11:42:19 +0530

    Time: 8 ms (78.26%), Space: 19.2 MB (84.64%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int countCollisions(string directions) {
-        int lc = 0;
-        int rc = 0;
-        int sc = 0;
         int n = directions.length();
+        int lc = 0;
+        int rc = n;
+        int sc = 0;
         for (int i = 0; i < n; i++) {
             if (directions[i] == 'L') lc ++;
             else break;
         }
         for (int i = n - 1; i > -1; i--) {
-            if (directions[i] == 'R') rc ++;
+            if (directions[i] == 'R') rc --;
             else break;
         }
-        for (int i = n - 1; i > -1; i--) {
-            if (directions[i] == 'S') sc ++;
+        for (int i = lc; i < rc ; i++) {
+            if (directions[i] != 'S') sc ++;
         }
-        return n - lc - rc - sc;
+        return sc;
     }
 };
