19c1c6769aa037903e09b72adc8b5aaf9d140245
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-12-04 11:33:54 +0530

    Time: 7 ms (95.91%), Space: 18.2 MB (70.26%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.py
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.py
@@ -1,24 +1,4 @@
 class Solution:
     def countCollisions(self, directions: str) -> int:
-        res = 0
-        st = []
-        for i in directions:
-            if i == 'R':
-                st.append(i)
-            elif i == 'L':
-                if st:
-                    if st.pop() == 'R':
-                        res += 2
-                        while st and st[-1] == 'R':
-                            res += 1
-                            st.pop()
-                    else:
-                        res += 1
-                    st.append('S')
-            else:
-                while st and st[-1] == 'R':
-                    res += 1
-                    st.pop()
-                st.append(i)
-            # print(res)
-        return res
+        s = directions.lstrip('L').rstrip('R')
+        return len(s) - s.count('S')
