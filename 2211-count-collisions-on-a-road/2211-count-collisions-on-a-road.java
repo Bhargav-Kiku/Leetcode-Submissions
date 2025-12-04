171fb7ea398ef0c70ff7e7b8b9b43e225c0314fc
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-12-04 11:39:23 +0530

    Time: 10 ms (93.15%), Space: 47.3 MB (42.74%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
@@ -3,17 +3,18 @@ class Solution {
         int lc = 0;
         int rc = 0;
         int sc = 0;
-        for (int i = 0; i < directions.length(); i++) {
+        int n = directions.length();
+        for (int i = 0; i < n; i++) {
             if (directions.charAt(i) == 'L') lc ++;
             else break;
         }
-        for (int i = directions.length() - 1; i > -1; i--) {
+        for (int i = n - 1; i > -1; i--) {
             if (directions.charAt(i) == 'R') rc ++;
             else break;
         }
-        for (int i = directions.length() - 1; i > -1; i--) {
+        for (int i = n - 1; i > -1; i--) {
             if (directions.charAt(i) == 'S') sc ++;
         }
-        return directions.length() - lc - rc - sc;
+        return n - lc - rc - sc;
     }
 }
