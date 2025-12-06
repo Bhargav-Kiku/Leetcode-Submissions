9b0f0a2895268dac128302b04233cb063e0c2d5f
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-12-07 00:55:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3765-complete-prime-number/3765-complete-prime-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    private HashMap<Integer, Boolean> p = new HashMap<>();
+    private Boolean isPrime(int x) {
+        if (p.containsKey(x)) return p.get(x);
+        if (x == 0) return true;
+        if (x == 1) return false;
+        for (int i = 2; i <= Math.sqrt(x); i++) {
+            if (x % i == 0) {
+                p.put(x,false);
+                return false;
+            }
+        }
+        p.put(x,true);
+        return true;
+    }
+    public boolean completePrime(int num) {
+        int tn = 10;
+        while (tn <= num) {
+            int x = num % tn;
+            tn *= 10;
+            if (!isPrime(x)) return false;
+        }
+        while (tn > 0) {
+            int x = num % tn;
+            tn /= 10;
+            if (!isPrime(x)) return false;
+        }
+        return true;
+    }
+}
