ea44a9751cd6489a9ba56a049363ecbd3f618da6
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2025-12-25 13:40:05 +0530

    Time: 790 ms (89.36%), Space: 43.4 MB (95.74%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
@@ -1,12 +1,11 @@
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        n = len(happiness)
         happiness.sort(reverse = True)
         res = 0
         t = 0
-        for i in happiness:
-            res += max(0, i - t)
-            if t < k - 1:
-                t += 1
-            else:
+        for i in range(k):
+            g = max(0, happiness[i] - i)
+            if g <= 0:
                 return res
+            res += g
+        return res
