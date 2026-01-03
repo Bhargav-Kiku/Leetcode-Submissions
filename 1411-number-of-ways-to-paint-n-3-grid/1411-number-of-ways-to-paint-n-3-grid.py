92cd99247bd237d6746c3ab1cfc9fab7ed4e2676
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2026-01-03 13:00:55 +0530

    Time: 33 ms (37.5%), Space: 17.4 MB (93.29%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.py
@@ -0,0 +1,7 @@
+MOD = 10**9 + 7
+class Solution:
+    def numOfWays(self, n: int) -> int:
+        a121, a123 = 6, 6
+        for i in range(n - 1):
+            a121, a123 = a121 * 3 + a123 * 2, a121 * 2 + a123 * 2
+        return (a121 + a123) % MOD
