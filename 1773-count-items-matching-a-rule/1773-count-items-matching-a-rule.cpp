b1ce38150d44660e2a4eb7378642e6aa23646318
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2026-01-06 22:49:50 +0530

    Time: 0 ms (100%), Space: 36.2 MB (86.62%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -5,10 +5,8 @@ public:
         int toc = 0;
         if (ruleKey == "color") toc = 1;
         else if (ruleKey =="name") toc = 2;
-        for (vector<string> pair: items) {
-            if (pair[toc] == ruleValue) {
-                res += 1;
-            }
+        for (int i = 0; i < items.size(); i++) {
+            if (items[i][toc] == ruleValue) res++;
         }
         return res;
     }
