a49b7f22f3bae56809e50a185671590bfe293428
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2026-01-12 09:19:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (6.71%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
@@ -3,18 +3,7 @@ class Solution:
         res = 0
         cx, cy = points[0]
         for i, j in points[1:]:
-            while cx != i and cy != j:
-                if cx < i:
-                    cx += 1
-                else:
-                    cx -= 1
-                if cy < j:
-                    cy += 1
-                else:
-                    cy -= 1
-                res += 1
-            res += abs(cx - i) + abs(cy - j)
-            # print(res)
+            res += max(abs(cx-i),abs(cy-j))
             cx = i
             cy = j
         return res
