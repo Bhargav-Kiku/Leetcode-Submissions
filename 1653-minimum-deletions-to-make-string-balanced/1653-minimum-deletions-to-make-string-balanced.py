11e3f3f81bea8c2b07bf20ad715863c61c1ec32e
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2026-02-07 19:26:10 +0530

    Time: 283 ms (53.7%), Space: 20.5 MB (33.42%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,21 +1,14 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
         n = len(s)
-        pre = [0] * (n + 1)
-        suf = [0] * (n + 1)
-        for i in range(1,n + 1):
-            if s[i - 1] == 'b':
-                pre[i] = pre[i-1] + 1
-            else:
-                pre[i] = pre[i-1]
-        for i in range(n - 1, -1, -1):
+        if 'a' not in s or 'b' not in s: return 0
+        pre = 0
+        suf = s.count('a')
+        res = suf
+        for i in range(n):
             if s[i] == 'a':
-                suf[i] = suf[i+1] + 1
+                suf -= 1
             else:
-                suf[i] = suf[i+1]
-        res = n
-        # print(*pre)
-        # print(*suf)
-        for i in range(n+1):
-            res = min(res, pre[i] + suf[i])
+                pre += 1
+            res = min(res, pre + suf)
         return res
