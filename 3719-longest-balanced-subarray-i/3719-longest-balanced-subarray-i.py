19b48b116f116f06326301d9d636f137d6b633bd
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2026-02-10 09:51:51 +0530

    Time: 279 ms (99.57%), Space: 19.7 MB (13.62%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.py
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.py
@@ -2,13 +2,16 @@ class Solution:
     def longestBalanced(self, nums: List[int]) -> int:
         res = 0
         n = len(nums)
+        vis = set()
         for i in range(n):
-            e = set()
-            o = set()
+            vis.clear()
+            bal = 0
+            if res > n - i: break
             for j in range(i,n):
-                if nums[j] & 1: o.add(nums[j])
-                else: e.add(nums[j])
-                if len(e) == len(o):
-                    if j - i + 1 > res:
-                        res = j - i + 1
+                if nums[j] not in vis:
+                    if nums[j] & 1: bal += 1
+                    else: bal -= 1
+                    vis.add(nums[j])
+                if bal == 0:
+                    res = max(res, j - i + 1)
         return res
