d0cf2a4b477787bdc4eab5da58f881bff34cd19f
Bhargav Kikani <134376536+Bhargav-Kiku@users.noreply.github.com>
2026-06-24 22:52:04 +0530

    Time: 8027 ms (48.28%), Space: 20.5 MB (62.07%) - LeetHub

---
+++ 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.py
@@ -0,0 +1,40 @@
+MOD = 1000000007
+class Solution:
+    def mul(self, a, b):
+        n = len(a)
+        m = len(b[0])
+        res = [[0] * m for _ in range(n)]
+        for i in range(n):
+            for k in range(len(a[0])):
+                r = a[i][k]
+                if r == 0:
+                    continue
+                for j in range(m):
+                    res[i][j] = (res[i][j] + r * b[k][j]) % MOD
+        return res
+
+    def powMul(self, base, exp, res):
+        while exp > 0:
+            if exp & 1:
+                res = self.mul(res, base)
+            base = self.mul(base, base)
+            exp >>= 1
+        return res
+
+    def zigZagArrays(self, n: int, l: int, r: int) -> int:
+        m = r - l + 1
+        if n == 1:
+            return m
+        size = 2 * m
+        u = [[0] * size for _ in range(size)]
+        for i in range(m):
+            for j in range(i):
+                u[i][j + m] = 1
+            for j in range(i + 1, m):
+                u[i + m][j] = 1
+        dp = [[1] * size]
+        dp = self.powMul(u, n - 1, dp)
+        res = 0
+        for i in range(size):
+            res = (res + dp[0][i]) % MOD
+        return res
